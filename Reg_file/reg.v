aa6bdb6f445765c2a46e72522e004521b2c7ad25
Pavani <pavanimalchuru@gmail.com>
2026-02-10 18:02:07 +0530

    Register File done!!

---
+++ Reg_file/reg.v
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+module reg_file (
+    input clk,
+    input reset,
+    input [4:0] read_reg1, read_reg2, write_reg,
+    input [63:0] write_data,
+    input reg_write_en,
+    output [63:0] read_data1, read_data2
+);
+// declaring the register file as 32 element array of 64-bit registers and following the big endian convention
+reg [63:0] registers [31:0];
+// read operation
+assign read_data1 = registers[read_reg1];
+assign read_data2 = registers[read_reg2];
+// write operation
+integer i;
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        for (i = 0; i < 32; i = i + 1) begin
+            registers[i] <= 64'b0;
+        end
+    end else if (reg_write_en) begin
+        registers[write_reg] <= write_data;
+    end
+end
+
+endmodule
