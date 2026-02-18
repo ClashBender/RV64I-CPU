ba53d2f6705f52e36561aee1f343391f32fdc2d2
Pavani <pavanimalchuru@gmail.com>
2026-02-18 16:25:56 +0530

    Modified imm generation

--- Imm_gen/imm.v
+++ Imm_gen/imm.v
@@ -6,12 +6,13 @@ module imm(
 );
     always @(*) begin 
         case (instruction[6:0])
-            7'b0010011: imm_out = {{52{instruction[31]}}, instruction[31:20]}; // I-type
-            7'b0000011: imm_out = {{52{instruction[31]}}, instruction[31:20]}; // Load
+            7'b0010011: imm_out = {{52{instruction[31]}}, instruction[31:20]}; // I-type (arithmetic)
+            7'b0000011: imm_out = {{52{instruction[31]}}, instruction[31:20]}; // I-type (Load)
+            7'b1100111: imm_out = {{52{instruction[31]}}, instruction[31:20]}; // I-type (Jump)
             7'b0100011: imm_out = {{52{instruction[31]}}, instruction[31:25], instruction[11:7]}; // S-type
             7'b1100011: imm_out = {{52{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0}; // B-type
             7'b1101111: imm_out = {{44{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0}; // J-type
             default: imm_out = 64'h0;
         endcase 
     end 
-endmodule
+endmodule 
