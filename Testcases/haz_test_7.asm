048bb774028b659e97f598298e380adf757784fd
Nikhilesh Nallavelli <nikhilesh.nallavelli@gmail.com>
2026-03-10 17:03:08 +0530

    added jal test (while loop)

---
+++ Testcases/haz_test_7.asm
@@ -0,0 +1,12 @@
+# Tests jal instruction and control hazard
+addi x5 x0 5
+addi x4 x0 2
+beq x5 x4 12 # while loop condition
+addi x4 x0 1 # increments x4 by 1
+jal x20 -4 # x20 = 12, PC = PC + 20
+add x6 x5 x4
+
+# dummy
+addi x0 x0 0
+addi x0 x0 0
+addi x0 x0 0
