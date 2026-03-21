c0da5e143e52f08c0aa446e5c9d5ff9b26e8900a
Nikhilesh Nallavelli <nikhileshnallavelli@gmail.com>
2026-03-21 13:24:09 +0530

    added the classify module

---
+++ src/core/fpu/classify.v
@@ -0,0 +1,47 @@
+module classifyFD {
+    input mode // 0 for FP32, 1 for FP64
+    input [63:0] a,
+    output [9:0] classify
+    output nan, inf, zero, subn, normal;
+};
+
+
+
+always @(*) begin
+    
+    if(mode) begin
+        if(a[62:52] == 11'hfff) begin // Exponent all 1s
+            if(a[51:0] == 0) classify = a[63] ? 10b'00_0000_0001 : 10b'00_1000_0000; // Infinity
+            else if(a[51] == 1) classify = 10b'01_0000_0000; // sNaN
+            else classify = 10b'10_0000_0000; // qNaN
+        end
+        else if(a[62:52] == 0) begin // Exponent all 0s
+            if(a[51:0] == 0) classify = a[63] ? 10b'00_0000_1000 : 10b'00_0001_0000; // Zero
+            else classify = a[63] ? 10b'00_0000_0100 : 10b'00_0010_0000; // Subnormal
+        end
+        else classify = a[63] ? 10b'00_0000_0010 : 10b'00_0100_0000; // Normal
+    end
+    else begin
+        if(a[30:23] == 8'hff) begin // Exponent all 1s
+            if(a[22:0] == 0) classify = a[31] ? 10b'00_0000_0001 : 10b'00_1000_0000; // Infinity
+            else if(a[22] == 1) classify = 10b'01_0000_0000; // sNaN
+            else classify = 10b'10_0000_0000; // qNaN
+        end
+        else if(a[30:23] == 0) begin // Exponent all 0s
+            if(a[22:0] == 0) classify = a[31] ? 10b'00_0000_1000 : 10b'00_0001_0000; // Zero
+            else classify = a[31] ? 10b'00_0000_0100 : 10b'00_0010_0000; // Subnormal
+        end
+        else classify = a[31] ? 10b'00_0000_0010 : 10b'00_0100_0000; // Normal
+    end
+        
+    nan = classify[9] | classify[8];
+    inf = classify[7] | classify[0];
+    zero = classify[4] | classify[3];
+    subn = classify[5] | classify[2];
+    normal = classify[6] | classify[1];
+    
+end
+
+
+
+endmodule
